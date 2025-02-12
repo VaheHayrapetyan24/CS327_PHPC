50b638f7297d73a718715f6d67182eda0780c34f
VaheHayrapetyan24 <vahe.hayrapetyanusa@gmail.com>
2025-02-12 20:55:08 +0400

    fix: free args memory

--- hw1_2/taylor_sin_threads.c
+++ hw1_2/taylor_sin_threads.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <pthread.h>
+#include <time.h>
 
 #define NUM_OF_ITERATIONS 50000
 #define NUM_OF_THREADS 10
@@ -15,16 +16,12 @@ struct thread_arguments {
 long double next_term(double x, long double previous_term, int powr) {
 	int original_powr = powr;
 	while (powr > 0 && powr > original_powr - 2 * NUM_OF_THREADS) {
-//		printf("powr %d, term %.30Lf\n", powr, previous_term);
-		
 		previous_term *= x/powr;
-//		printf("asdf %.30Lf\n", previous_term);
 		--powr;
 	}
 
 	long double result = (((original_powr - 1) / 2) % 2 ? -1 : 1) * fabs(previous_term);
 
-//	printf("before asdf %.30Lf, %.30Lf, %d\n", previous_term, result, isinf(result));
 	return isinf(result) ? 0.0L : result;
 }
 
@@ -36,10 +33,8 @@ void* calculate_sum(void* args) {
 
 	int powr = 2*(cast_args -> thread_index) + 1;
 	long double current = next_term(X, 1.0L, powr);
-//	printf("first current %.30Lf\n", current);
 	for (int i = cast_args -> thread_index; i < NUM_OF_ITERATIONS; i+= NUM_OF_THREADS) {
 		*(cast_args -> sum) += current;
-//		printf("summed %.30Lf, %.30Lf\n", *(cast_args -> sum), current);
 		powr += 2 * NUM_OF_THREADS;
 		current = next_term(X, current, powr);
 	}
@@ -55,13 +50,17 @@ int main() {
 	
 	long double sums[NUM_OF_THREADS] = {0.0L};
 	pthread_t threads[NUM_OF_THREADS];
+	struct thread_arguments* args[NUM_OF_THREADS];
 
 	for (int i = 0; i < NUM_OF_THREADS; ++i) {
-		struct thread_arguments *args = (struct thread_arguments*) malloc(sizeof(struct thread_arguments));
-		args->thread_index = i;
-		args->sum = &sums[i];
+		// I decided to have args array to be able to free the memory afterwards
+		// Otherwise I would have to call free in calculate_sum, which seems wrong since
+		// the args is created in this scope, so in this scope it should be deleted
+		args[i] = (struct thread_arguments*) malloc(sizeof(struct thread_arguments));
+		args[i]->thread_index = i;
+		args[i]->sum = &sums[i];
 		
-		int res = pthread_create(&(threads[i]), NULL, calculate_sum, (void*) args);
+		int res = pthread_create(&(threads[i]), NULL, calculate_sum, (void*) args[i]);
 		if (res != 0) {
 			printf("failed to create thread: %d\n", i);
 			return -1;
@@ -74,15 +73,9 @@ int main() {
 	for (int i = 0; i < NUM_OF_THREADS; ++i) {
 		pthread_join(threads[i], NULL);
 		sum += sums[i];
+		free(args[i]);
 		printf("sum %.30Lf\n", sums[i]);
 	}
 
 	printf("sin(%f) = %.30Lf\n", X, sum); 
-
-
-
-	
-//	printf("%.30Lf\n", next_term(1, 1.0L/6, 13));
-
-
 }
