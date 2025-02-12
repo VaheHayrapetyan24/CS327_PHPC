e4640a3f5a1b96bd03c79e2f49f390e1ba0e40b7
VaheHayrapetyan24 <vahe.hayrapetyanusa@gmail.com>
2025-02-12 23:00:42 +0400

    fix: use time of day instead of clock timer

--- hw1_2/taylor_sin_threads_better.c
+++ hw1_2/taylor_sin_threads_better.c
@@ -4,8 +4,8 @@
 #include <pthread.h>
 #include <time.h>
 
-#define NUM_OF_ITERATIONS 500000000
-#define NUM_OF_THREADS 1
+#define NUM_OF_ITERATIONS 5000000000
+#define NUM_OF_THREADS 50
 #define X 0.3L
 
 struct thread_arguments {
@@ -19,19 +19,16 @@ struct thread_arguments {
 
 void* calculate_sum(void* args) {
 	struct thread_arguments *cast_args = (struct thread_arguments*) args;
-//	printf("running in thread %d, sum is %Lf \n", cast_args -> thread_index, (cast_args -> sum));
 
 
-	int start = cast_args -> thread_index * NUM_OF_ITERATIONS / NUM_OF_THREADS;
-	int max = (cast_args -> thread_index + 1) * NUM_OF_ITERATIONS / NUM_OF_THREADS;
-	long double current = X; // todo check if this works
-				 //
-//	printf("index %d, current %d, max %d, c %.30Lf\n", cast_args->thread_index, cast_args -> thread_index * NUM_OF_ITERATIONS / NUM_OF_THREADS, max, current);
+	long int start = cast_args -> thread_index * NUM_OF_ITERATIONS / NUM_OF_THREADS;
+	long int max = (cast_args -> thread_index + 1) * NUM_OF_ITERATIONS / NUM_OF_THREADS;
+	long double current = X;
+				 
 	
-	for (int i = start; i < max; ++i) {
+	for (long int i = start; i < max; ++i) {
 		cast_args -> sum = (current - cast_args -> sum);
 		current *= X * X / ((i + 1) * 2 * ((i + 1) * 2 + 1));
-//		printf("cur res %.30Lf %d\n", X * X, i * 2 * (i * 2 + 1));
 	}
 	cast_args -> sum *= -1;
 
@@ -45,16 +42,13 @@ void* calculate_sum(void* args) {
 
 
 int main() {
-	clock_t start = clock();
+	struct timespec start, end;
+	clock_gettime(CLOCK_MONOTONIC, &start);
 
-	// long double sums[NUM_OF_THREADS] = {0.0L};
 	pthread_t threads[NUM_OF_THREADS];
 	struct thread_arguments* args[NUM_OF_THREADS];
 
 	for (int i = 0; i < NUM_OF_THREADS; ++i) {
-		// I decided to have args array to be able to free the memory afterwards
-		// Otherwise I would have to call free in calculate_sum, which seems wrong since
-		// the args is created in this scope, so in this scope it should be deleted
 		args[i] = (struct thread_arguments*) malloc(sizeof(struct thread_arguments));
 		args[i]->thread_index = i;
 		args[i]->sum = 0.0L;
@@ -71,16 +65,15 @@ int main() {
 	long double sum = 0.0;
 	for (int i = 0; i < NUM_OF_THREADS; ++i) {
 		pthread_join(threads[i], NULL);
-//		printf("res %.30Lf\n", args[i] -> sum);
 
 		sum += args[i]->sum * (i == 0 ? 1 : args[i - 1] -> biggest_term);
 		free(args[i]);
-//		printf("sum %.30Lf\n", sums[i]);
 	}
 
 	printf("sin(%Lf) = %.30Lf\n", X, sum); 
 
+	clock_gettime(CLOCK_MONOTONIC, &end);
 
 
-	printf("Threads: %d, Total iterations: %d, Time: %ld microseconds\n", NUM_OF_THREADS, NUM_OF_ITERATIONS, (int) clock() - start);
+	printf("Threads: %3d, Total iterations: %ld, Time: %Lf nanoseconds\n", NUM_OF_THREADS, NUM_OF_ITERATIONS, (long double) (end.tv_sec - start.tv_sec) * 10e9 + (end.tv_nsec - start.tv_nsec));
 }
