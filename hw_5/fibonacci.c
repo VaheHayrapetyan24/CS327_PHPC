e03f879df8fc04fdb3bf4593170c93415975db24
VaheHayrapetyan24 <vahe.hayrapetyanusa@gmail.com>
2025-04-05 19:25:45 +0400

    fix: clean up

--- hw_5/fibonacci.c
+++ hw_5/fibonacci.c
@@ -12,109 +12,7 @@ int fibbonacci_naive(int n) {
     return fibbonacci_naive(n - 1) + fibbonacci_naive(n - 2);
 }
 
-
-
-int fibbonaci_wait_tasks(int n) {
-    if (n <= 10) {
-        return fibbonacci_naive(n);
-    }
-
-    int res;
-    #pragma omp taskwait // this does nothing
-    {
-        res = fibbonacci_naive(n - 1) + fibbonacci_naive(n - 2);
-    }
-
-    return res;
-}
-
-int fibbonaci_sequential_parallel(int n) {
-    int res;
-    #pragma omp parallel num_threads(NUM_THREADS)
-    {
-        res = fibbonaci_wait_tasks(n);
-    }
-    return res;
-}
-
-
-// this is good version
-// int fibbonaci_branched_tasks(int n) {
-//     if (n <= 10) {
-//         return fibbonacci_naive(n);
-//     }
-
-//     int sum = 0;
-
-//     #pragma omp task shared(sum)
-//     {
-//         // printf("sum task 1 before %d\n", sum);
-//         #pragma omp atomic
-//         sum += fibbonaci_branched_tasks(n - 1);
-//         // printf("sum task 1 after %d\n", sum);
-//     }
-
-//     #pragma omp task shared(sum)
-//     {
-//         // printf("sum task 2 before %d\n", sum);
-//         #pragma omp atomic
-//         sum += fibbonaci_branched_tasks(n - 2);
-//         // printf("sum task 2 after %d\n", sum);
-//     }
-
-    
-
-//     #pragma omp taskwait
-
-//     // printf("sum %d\n", sum);
-
-
-//     return sum;
-// }
-
-// int fibbonaci_branched(int n) {
-//     int res;
-//     #pragma omp parallel num_threads(NUM_THREADS)
-//     {
-//         #pragma omp single
-//         res = fibbonaci_branched_tasks(n);
-
-//     }
-//     return res;
-// }
-
-
-// this is what the idiot did
-// int fibbonaci_branched_tasks(int n) {
-//     if (n <= 10) {
-//         return fibbonacci_naive(n);
-//     }
-
-//     int x = 0, y = 0;
-
-//     #pragma omp task shared(x)
-//     x = fibbonaci_branched_tasks(n - 1);
-
-//     #pragma omp task shared(y)
-//     y = fibbonaci_branched_tasks(n - 2);
-
-//     #pragma omp taskwait
-//     return x + y;
-// }
-
-// int fibbonaci_branched(int n) {
-//     int res = 0;
-
-//     #pragma omp parallel
-//     {
-//         #pragma omp single
-//         res = fibbonaci_branched_tasks(n);
-//     }
-
-//     return res;
-// }
-
-int fibbonaci_branched_tasks(int n) {
+int fibbonaci_tasks(int n) {
     if (n <= 10) {
         return fibbonacci_naive(n);
     }
@@ -124,7 +22,7 @@ int fibbonaci_branched_tasks(int n) {
     for (int i = 1; i <= 2; ++i) {
         #pragma omp task shared(vals)
         {
-            vals[i - 1] += fibbonaci_branched_tasks(n - i);
+            vals[i - 1] += fibbonaci_tasks(n - i);
         }
     }
 
@@ -133,12 +31,12 @@ int fibbonaci_branched_tasks(int n) {
     return vals[0] + vals[1];
 }
 
-int fibbonaci_branched(int n) {
+int fibbonaci_parallel(int n) {
     int res;
     #pragma omp parallel num_threads(NUM_THREADS)
     {
         #pragma omp single
-        res = fibbonaci_branched_tasks(n);
+        res = fibbonaci_tasks(n);
     }
     return res;
 }
@@ -153,25 +51,17 @@ double get_time_in_seconds() {
 
 
 int main() {
-    // for (int i = 5; i < 6; ++i) 
+    int n = 45;
 
-    int n = 50;
     double start = get_time_in_seconds();
-    // int fib = fibbonaci_sequential_parallel(n);
-    int fib;
-	
-	double end = get_time_in_seconds();
-    printf("fibb parallel %d %f \n", fib, (end - start));
-    
-    start = get_time_in_seconds();
-    // fib = fibbonacci_naive(n);
-    end = get_time_in_seconds();
+    int fib = fibbonacci_naive(n);
+    double end = get_time_in_seconds();
     printf("fibb naive %d %f \n", fib, (end - start));
 
 
 
     start = get_time_in_seconds();
-    fib = fibbonaci_branched_tasks(n);
+    fib = fibbonaci_tasks(n);
     end = get_time_in_seconds();
     printf("fibb branched %d %f \n", fib, (end - start));
 }
