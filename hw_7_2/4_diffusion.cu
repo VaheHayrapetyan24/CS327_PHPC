34e1ce334df9fb5cf56ebf50e7a42a6464f79898
VaheHayrapetyan24 <vahe.hayrapetyanusa@gmail.com>
2025-05-12 22:18:08 +0400

    feat: hw_7_4

---
+++ hw_7_2/4_diffusion.cu
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+#define BLOCKX 256
+#define ITER 100
+#define DECAY_RATE 0.01
+
+
+__global__ void diffusion(float* input, float* output, int n) {
+    int ind = blockDim.x * blockIdx.x + threadIdx.x + 2;
+
+    if (ind > n) return;
+
+    float val = (1 - DECAY_RATE) * (0.1 * (input[ind - 2] + input[ind + 2]) + 0.2 * (input[ind - 1] + input[ind + 1]) + 0.4 * input[ind]);
+
+    __syncthreads();
+    output[ind] = val;
+}
+
+
+void print_arr(float* arr, int n) {
+    printf("\n");
+    for (int i = 0; i < n; ++i) {
+        printf("%f ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int N = (1 << 20) + 4;
+
+    dim3 block, grid;
+    block.x = BLOCKX;
+    grid.x = (N + block.x - 5) / block.x;
+
+    float *h_input, *h_output;
+    float *d_input, *d_output;
+
+    size_t size = sizeof(int) * N;
+    cudaMallocHost(&h_input, size);
+    cudaMallocHost(&h_output, size);
+
+    for (int i = 0; i < N; ++i) {
+        h_input[i] = ((float) rand()) / rand();
+    }
+
+    cudaMalloc(&d_input, size);
+    cudaMalloc(&d_output, size);
+
+    cudaMemcpy(d_input, h_input, size, cudaMemcpyHostToDevice);
+
+    // On the first iteration this switches
+    float *t1 = d_output, *t2 = d_input;
+    for (int i = 0; i < ITER; ++i) {
+        float *temp = t1;
+        t1 = t2;
+        t2 = temp;
+        // printf("%p %p\n", t1, t2);
+        diffusion<<<grid, block>>>(t1, t2, N - 2);
+        cudaDeviceSynchronize();
+    }
+
+    cudaMemcpy(h_output, t2, size, cudaMemcpyDeviceToHost);
+
+    printf("input:\n");
+    for (int i = 0; i < N; ++i) {
+        printf(" %f", h_input[i]);
+    }
+    printf("\n");
+
+    printf("output:\n");
+    for (int i = 0; i < N; ++i) {
+        printf(" %f", h_output[i]);
+    }
+    printf("\n");
+
+    cudaFree(h_input);
+    cudaFree(h_output);
+    cudaFree(d_input);
+    cudaFree(d_output);
+
+}
